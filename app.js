ebdce6a72aec3d58503006456dcca5f36e0f8c98
Lisa Baetsle <lisa_baetsle@hotmail.com>
2020-11-30 11:26:36 +0100

    adding the landing page and animation

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Navigation
+
 function openNav() {
   document.getElementById("myNav").style.width = "100%";
 }
@@ -5,3 +7,27 @@ function openNav() {
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
+
+// Homepage overlay
+function removeOverlay() {
+  gsap.to(".homepage-overlay p", 2, {
+    opacity: 0,
+    y: -500,
+    ease: Circ.easeIn
+  });
+
+  gsap.to(".homepage-overlay img", 2, {
+    opacity: 0,
+    y: -500,
+    ease: Circ.easeIn
+  });
+
+  gsap.to(".homepage-overlay", 2, {
+    delay: 1,
+    top: "-100%",
+  })
+};
+
+document.getElementById('homepage-overlay').addEventListener('click', function(e) {
+  removeOverlay();
+})
